88baf187cdc7c1f554ac60bc5d8ca00f50165078
Alex Bergeron <alexwork1994@gmail.com>
2018-05-21 09:39:54 -0400

    mini update

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import os
 def msg_parser(message):            # for parsing the message content and writing it in a log file
     time_recv = strftime('|%Y-%m-%d %H:%M:%S|',gmtime())
     msg = [time_recv,message.channel,message.author,message.content]
-    print('{}~{}~| {}: {} \n'.format(*msg))
+    print('{}~{}~| {}: {} '.format(*msg))
     file = open('discord_channel.log','a+')
     file.write('{}~{}~| {}: {} \n'.format(*msg))
     file.close()
