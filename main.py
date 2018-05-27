781173085386ced860624d7a15620d78229593e9
Alex Bergeron <alexwork1994@gmail.com>
2018-05-27 14:11:59 -0400

    minor fix

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import discord
 import token_babo
 from utils import Utils
+from Commands import *
+import asyncio
 
 
 class MyClient(discord.Client):
+    commands = dict()
+    commands["ping"] = PingCommand()
+    commands["clear"] = Clear()
+    commands['changeGame'] = ChangeGame()
 
     async def on_ready(self):
-        print('|logged in as {} .  The discord version is {}|'.format(self.user,discord.__version__))
+        print('|logged in as {} .  The discord version is {}|'.format(self.user, discord.__version__))
+        await client.change_presence(game=discord.Game(name="vec ma graine"))
 
-    async def on_message(self, message):
-        print(Utils.msg_parser(message))
-        with open('text_log.txt','a') as file:
-            file.write(Utils.msg_parser(message)+ '\n')
+    async def on_message(self, msg):
+        content = msg.content
+        print(Utils.msg_parser(msg))
+        with open('text_log.txt', 'a') as file:
+            file.write(Utils.msg_parser(msg) + '\n')
             file.close()
-            if message.content == '$babo':
-                await message.channel.send(Utils.q_google('dindon'))
+            if content == '$babo':
+                await msg.channel.send(Utils.q_google('dindon'))
 
-        parser = Utils(message)
-        if "!" in message.content:
-            parser.cmd_parser()
+        parser = Utils(msg)
+        if "!" in content:
+            cmd = parser.cmd_parser()
+            if cmd.invoke in content:
+                await self.commands[cmd.invoke].action(msg=msg, args=cmd.args)
+
+        else:
+            parser.msg_parser()
 
 
 client = MyClient()
-client.run(token_babo.token)
