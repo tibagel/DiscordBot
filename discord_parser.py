7c728932e7119e3be318625927579b740cca453f
Dannick Larochelle <dann.laro@hotmail.com>
2018-05-27 01:21:23 -0400

    just restarted

--- discord_parser.py
+++ discord_parser.py
@@ -1,9 +1,29 @@
 from time import gmtime, strftime
 
+
 class Discord_parser():
 
-    def msg_parser(message):
+    def __init__(self, msg):
+        self.channel = msg.channel
+        self.author = msg.author
+        self.content = msg.content
+
+    def msg_parser(self):
         time_recv = strftime('|%Y-%m-%d %H:%M:%S|', gmtime())
-        msg = [time_recv, message.channel, message.author, message.content]
+        msg = [time_recv, self.channel, self.author, self.content]
         formatted_msg = ('{}~{}~| {}: {} '.format(*msg))
         return formatted_msg + "\n"
+
+    def cmd_parser(self):
+        content = self.content
+        no_head = content.replace("!", "")
+        no_head_split = no_head.split(" ")
+        invoke = no_head_split[0]
+        raw_args = no_head.replace(invoke, "")
+        print(raw_args)
+        if '"' in raw_args:
+            splitted = str(no_head_split).split('"')
+        else:
+            splitted = str(no_head_split).split(" ")
+
+        print(splitted)
--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import discord
 import token_babo
 from discord_parser import Discord_parser
 
+
 class MyClient(discord.Client):
 
     async def on_ready(self):
-        print('logged in as {} .  The discord version is {}'.format(self.user,discord.__version__))
+        print('logged in as {} .  The discord version is {}'.format(self.user, discord.__version__))
 
     async def on_message(self, message):
-        print(Discord_parser.msg_parser(message))
+        parser = Discord_parser(message)
+        if "!" in message.content:
+            parser.cmd_parser()
+        else:
+            print(parser.msg_parser())
+
 
 client = MyClient()
 client.run(token_babo.token)
