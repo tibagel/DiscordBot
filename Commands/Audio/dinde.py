f91e32342460719d02ff7bbfaa61a026bf3231c0
Dannick Larochelle <dann.laro@hotmail.com>
2020-02-08 17:22:13 -0500

    Fixed the whole text and image logging ... !

--- Commands/Audio/dinde.py
+++ Commands/Audio/dinde.py
@@ -2,14 +2,15 @@ from ctypes.util import find_library
 from Commands.commands import Commands
 import discord
 from Commands.Audio.voice_utils import VoicePlayer
+from discord import opus
 
 client = discord.Client()
 lib = find_library('opus')
-discord.opus.load_opus(lib)
+opus.load_opus(lib)
 
 
 class Dinde(Commands):
     async def action(self, msg, args, client):
         vp = VoicePlayer(msg)
         await vp.join_voice()
-        await vp.file_play('Turkey.mp3')
+        await vp.file_play('Turkey.m4a')
